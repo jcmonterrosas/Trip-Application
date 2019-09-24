addb942906b5743b00f021b41c69ae2e00504eb2
jcmonterrosas <38740763+jcmonterrosas@users.noreply.github.com>
2019-09-24 11:30:49 -0500

    Se agregan caracteristicas al formulario (NewTrip)

--- grails-app/domain/trip/planner/Trip.groovy
+++ grails-app/domain/trip/planner/Trip.groovy
@@ -2,6 +2,17 @@ package trip.planner
 
 class Trip {
 
+    String destino
+    String espectativas
+    String actividades
+    Integer cantidadDias
+    Date diaInicio
+    Date diaFin   
+
     static constraints = {
+        actividades widget: 'textarea'
+        espectativas nullable: true, widget: 'textarea'
+        diaInicio nullable: true
+        diaFin nullable: true
     }
 }
